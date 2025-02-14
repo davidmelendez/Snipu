e6795276abb6df4ee7f43fa67efa5986b6633475
sudiptapaul <psudipta858@gmail.com>
2025-02-14 23:22:42 +0530

    Server actions on newsnippet and home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+
+  const snippets = await prisma.snippet.findMany();
   return (
     <div>
       <h1 className="font-bold text-4xl">Home</h1>
       <div className="flex items-center justify-between">
         <h1>Snippets</h1>
         <Link href={"/snippet/new"}><Button>New Snippet</Button></Link>
-        
       </div>
+      {snippets.map((snippet) => (
+         <div key={snippet.id} className="flex items-center justify-between bg-gray-300 p-2 rounded-md my-4">
+          <h1>{snippet.title}</h1>
+          <Link href={`/snippet/${snippet.id}`}><Button variant={"link"}>View</Button></Link>
+         </div>
+      ))}
     </div>
   );
 }
--- src/app/snippet/new/page.tsx
+++ src/app/snippet/new/page.tsx
@@ -2,11 +2,30 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
+import { prisma } from '@/lib/prisma'
+import { redirect } from 'next/navigation'
+
 
 
 const CreateSnippetPage = () => {
+    async function createSnippet(formData: FormData) {
+      "use server"
+      const title = formData.get('title') as string;
+      const code = formData.get('code') as string;
+
+      const snippet = await prisma.snippet.create({
+        data: {
+          title,
+          code
+        }
+      })
+      console.log("Snippet created", snippet);
+      
+      redirect("/");
+    }
+
   return (
-    <form>
+    <form action={createSnippet}>
         <div>
             <Label>Title</Label>
             <Input type="text" name='title' id='title'/>
