4c393f7513131cea1f7eb6587756bb49224350a3
gchyuli <ygonzalez1910@gmail.com>
2025-05-02 09:34:48 -0600

    conflicts resolved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+  
         <StarknetProvider>
           <WalletProvider>{children}</WalletProvider>
         </StarknetProvider>
